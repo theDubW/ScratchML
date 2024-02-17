4d8466404c7c470c5678c6fd11cf013375cac4ac
Yatin Kukreja <ykukreja@purdue.edu>
2024-02-17 04:14:17 -0800

    routing and nav bar

--- frontend/src/views/landing_page.jsx
+++ frontend/src/views/landing_page.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import TaskList from '../components/TaskList';
 import Map from '../components/map';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 const LandingPage = () => {
     return (
-        <Route exact path="/">
-        <div className='flex flex-row p-5'>
+        <div className='flex flex-row p-2'>
             <TaskList className='h-full'/>
             <Map className=''/>
         </div>
-        </Route>
     )
 };
 
