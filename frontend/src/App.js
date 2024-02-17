e0c31b533f4986ad00dcdafaba69f7091909f2ba
Yatin Kukreja <ykukreja@purdue.edu>
2024-02-16 21:50:44 -0800

    added chackra

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
+    <ChakraProvider>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +21,7 @@ function App() {
         </a>
       </header>
     </div>
+    </ChakraProvider>
   );
 }
 
