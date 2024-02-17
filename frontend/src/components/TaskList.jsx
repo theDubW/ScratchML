93e85cc77e4697ea816f1e7a5b74074af18e5c18
Yatin Kukreja <ykukreja@purdue.edu>
2024-02-17 00:49:36 -0800

    set up task list and map skeleton

--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
     Accordion,
     AccordionItem,
@@ -8,78 +8,54 @@ import {
     Box,
 } from '@chakra-ui/react'
 
-
-export class task{
-    constructor(title, description, id) {
-        this.title = title;
-        this.description = description;
-        this.id = id;
-    }
-}
-
-
-
 const TaskList = () => {
 
-    const [tasks, setTasks] = useState([new task("test", "test descritption", "id")]);
-    
     return (
-        <>
-        <Accordion allowToggle>
-        {tasks.map((task, index) => {
-            <>
-            <AccordionItem>
+        <div className="basis-2/5 h-full border-2 rounded m-2">
+        <Accordion allowToggle >
+            <AccordionItem className="m-2 border rounded">
                 <h2>
                     <AccordionButton>
                         <Box as="span" flex='1' textAlign='left'>
-                            {task.title}
+                            Regression
                         </Box>
                         <AccordionIcon />
                     </AccordionButton>
                 </h2>
-                <AccordionPanel pb={4}>
-                    {task.description}
-                </AccordionPanel>
-            </AccordionItem>
-            </>
-        })}
-</Accordion>
-        <Accordion allowToggle>
-            <AccordionItem>
-                <h2>
-                    <AccordionButton>
-                        <Box as="span" flex='1' textAlign='left'>
-                            Section 1 title
-                        </Box>
-                        <AccordionIcon />
-                    </AccordionButton>
-                </h2>
-                <AccordionPanel pb={4}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                    commodo consequat.
+                <AccordionPanel className="pb-4">
+                Regression is a statistical approach used to analyze the relationship between a dependent variable (target variable) and one or more independent variables (predictor variables). The objective is to determine the most suitable function that characterizes the connection between these variables.
                 </AccordionPanel>
             </AccordionItem>
 
-            <AccordionItem>
+            <AccordionItem className="m-2 border rounded">
                 <h2>
                     <AccordionButton>
                         <Box as="span" flex='1' textAlign='left'>
-                            Section 2 title
+                            Classification
                         </Box>
                         <AccordionIcon />
                     </AccordionButton>
                 </h2>
-                <AccordionPanel pb={4}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                    commodo consequat.
+                <AccordionPanel className="pb-4">
+                Classification is the task of “classifying things” into sub-categories. Classification is part of supervised machine learning in which we put labeled data for training.
+                </AccordionPanel>
+            </AccordionItem>
+
+            <AccordionItem className="m-2 border rounded">
+                <h2>
+                    <AccordionButton>
+                        <Box as="span" flex='1' textAlign='left'>
+                            Clustering
+                        </Box>
+                        <AccordionIcon />
+                    </AccordionButton>
+                </h2>
+                <AccordionPanel className="pb-4">
+                Clustering is the task of dividing the population or data points into a number of groups such that data points in the same groups are more similar to other data points in the same group and dissimilar to the data points in other groups. It is basically a collection of objects on the basis of similarity and dissimilarity between them. 
                 </AccordionPanel>
             </AccordionItem>
         </Accordion>
-        </>
+        </div>
     )
 };
 
---
+++ frontend/src/components/map.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const map = () => {
+    return (
+        <div className="basis-3/5 border-2 border-black rounded h-full m-2 bg-sky-500 hover:bg-sky-700">
+        Map goes here
+        </div>
+    )
+};
+
+export default map
