4e226d88148f16794c666076c82bbaaa465c8a63
Yatin Kukreja <ykukreja@purdue.edu>
2024-02-17 10:28:38 -0800

    slight ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import LandingPage from './views/landing_page';
 import lessonOne from './views/lesson_one';
 import { Button, Stack, ButtonGroup} from '@chakra-ui/react'
-import { IoMdSettings, IoMdPerson } from "react-icons/io";
 import { Heading } from '@chakra-ui/react'
 
 function App() {
@@ -14,10 +13,13 @@ function App() {
     <Stack direction='row' spacing={4} className="p-4 justify-between border-b">
     <Heading><Link to="/" className="font-lilitaOne">ScratchML</Link></Heading>
     <ButtonGroup spacing={4}>
-      <Button leftIcon={ <IoMdPerson/> } colorScheme='white'className="hover:bg-gray-300" variant='solid'>
+    <Button colorScheme='white'className="hover:bg-gray-300 border-blue-800 border-2 border-b-4" variant='solid'>
+        <text className="text-blue-800 font-lilitaOne">Sandbox</text>
+      </Button>
+      <Button colorScheme='white'className="hover:bg-gray-300 border-blue-800 border-2 border-b-4" variant='solid'>
         <text className="text-blue-800 font-lilitaOne">Account</text>
       </Button>
-      <Button leftIcon={<IoMdSettings />}colorScheme='white' className="hover:bg-gray-300 border-" variant='solid'>
+      <Button colorScheme='white' className="hover:bg-gray-300 border-blue-800 border-2 border-b-4" variant='solid'>
         <text className="text-blue-800 font-lilitaOne">Settings</text>
       </Button>
       </ButtonGroup>
