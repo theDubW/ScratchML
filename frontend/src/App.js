2e01b97478fe6e8921db6939d3f7e64b239179f7
Matthew Lee <matt.lee.hr@gmail.com>
2024-02-16 22:34:49 -0800

    added preliminary menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import { ChakraProvider } from '@chakra-ui/react'
+import TaskList from './components/TaskList'
+import { ChakraProvider, } from '@chakra-ui/react'
 
 function App() {
   return (
     <ChakraProvider>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <TaskList />
     </ChakraProvider>
   );
 }
