bd1114216ea50d3cdff400a450319dfd8f32a482
Matthew Lee <matt.lee.hr@gmail.com>
2024-02-17 09:16:37 -0800

    reshaping top row

--- frontend/src/components/level.jsx
+++ frontend/src/components/level.jsx
@@ -1,23 +1,24 @@
-import { Card, Container, Table, Thead, Tbody, Tr,Td, Th, Text, Box, CardHeader, CardBody, CardFooter, Flex, Button, TableContainer, TableCaption } from '@chakra-ui/react'
+import { Card, Container, Table, Thead, Tbody, Tr, Td, Th, Text, Box, CardHeader, CardBody, CardFooter, Flex, Button, TableContainer, TableCaption } from '@chakra-ui/react'
 import { DndContext, useDroppable, useDraggable } from '@dnd-kit/core';
 import { useEffect, useState } from 'react';
-import {getFirestore, onSnapshot,collection, doc} from "firebase/firestore";
-import {CSS} from '@dnd-kit/utilities';
-import {generateData} from '../helpers/callEndpoint';
+import { getFirestore, onSnapshot, collection, doc } from "firebase/firestore";
+import { CSS } from '@dnd-kit/utilities';
+import { generateData } from '../helpers/callEndpoint';
 
 const uid = "user_10";
-function Data(){
+
+function Data() {
   // listen to firestore for data
   const db = getFirestore();
   const [data, setData] = useState({});
-  
+
   useEffect(() => {
     const ref = doc(collection(doc(collection(db, "Users"), uid), "FoolsGold"), "train");
     onSnapshot(ref, (snapshot) => {
-      if(snapshot.exists()){
+      if (snapshot.exists()) {
         const docData = snapshot.data();
         const data = {};
-        for (const key in docData){
+        for (const key in docData) {
           data[key] = docData[key];
         }
         console.log(data);
@@ -32,18 +33,18 @@ function Data(){
     });
   }, []);
   return (
-    <Card className="w-full h-2/3 ml-3">
+    <Card id="data" className="w-full ml-3">
       <CardHeader className='text-center font-bold'>Data</CardHeader>
-        <CardBody className='text-center flex flex-col justify-end items-center'>
-          <TableContainer overflow="scroll" className='mt-0 mb-3'>
-            <Text className='text-bold'>Gold v. Fool's Gold Properties</Text>
-            <Table variant='simple' size="sm">
-              
-              <Tbody>
+      <CardBody className='text-center flex flex-col justify-end items-center'>
+        <TableContainer overflow="scroll" className='mt-0 mb-3'>
+          <Text className='text-bold'>Gold v. Fool's Gold Properties</Text>
+          <Table variant='simple' size="sm">
+
+            <Tbody>
               {
                 Object.keys(data).map((key) => {
-                  if(key !== "label"){
-                    
+                  if (key !== "label") {
+
                     const values = data[key];
 
                     return (
@@ -62,18 +63,18 @@ function Data(){
                   }
                 })
               }
-              </Tbody>
-            </Table>
+            </Tbody>
+          </Table>
 
-          </TableContainer>
-          <Button onClick={generateData(uid, "FoolsGold", 10)}>Generate Data</Button>
-        </CardBody>
+        </TableContainer>
+        <Button onClick={generateData(uid, "FoolsGold", 10)}>Generate Data</Button>
+      </CardBody>
     </Card>
   );
 }
 
-function Model({model}){
-  const {isOver, setNodeRef} = useDroppable({
+function Model({ model }) {
+  const { isOver, setNodeRef } = useDroppable({
     id: 'model-droppable',
   });
   const style = {
@@ -81,39 +82,34 @@ function Model({model}){
   };
 
   return (
-    <Card  className="w-2/3 flex-grow h-2/3 m-10" ref={setNodeRef} style={style}>
+    <Card className="w-1/3 h-full flex-grow m-10" ref={setNodeRef} style={style}>
       <CardHeader className='text-center font-bold'>Model</CardHeader>
-        <CardBody className='text-center '>
-          {model !== undefined ? model : <></>}
-        </CardBody>
+      <CardBody className='text-center '>
+        {model !== undefined ? model : <></>}
+      </CardBody>
     </Card>
   );
 }
-function Train(){
+
+function TrainRun() {
+
   return (
-    <Card  className="w-1/2 flex-grow h-2/3 m-10">
-      <CardHeader className='text-center font-bold'>Train</CardHeader>
-        <CardBody className='text-center flex justify-center items-center'>
-          {/* <Text className='font-bold'>Train</Text> */}
-          <Button>Train Model</Button>
-        </CardBody>
-    </Card>
-  );
-}
-function Run(){
-  return (
-    <Card className="w-1/2 flex-grow h-2/3 m-10">
-      <CardHeader className='text-center font-bold'>Run</CardHeader>
-        <CardBody className='text-center flex justify-center items-center'>
-          <Button className=''>Run Model</Button>
-        </CardBody>
+    <Card id="trainrun" className="w-1/3 h-full m-10 relative">
+      <CardHeader className='text-center font-bold'>Train / Run</CardHeader>
+      <CardBody className='text-center flex justify-center items-center'>
+        {/* <Text className='font-bold'>Train</Text> */}
+        <Button>Train Model</Button>
+      </CardBody>
+      <CardBody className='text-center flex justify-center items-center'>
+        <Button className=''>Run Model</Button>
+      </CardBody>
     </Card>
   );
 }
 
 
-function ModelOption({type}){
-  const {attributes, listeners, setNodeRef, transform} = useDraggable({
+function ModelOption({ type }) {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
     id: type,
   });
   const style = transform ? {
@@ -133,8 +129,8 @@ function ModelOption({type}){
   );
 }
 
-function FeatureOption({type}){
-  const {attributes, listeners, setNodeRef, transform} = useDraggable({
+function FeatureOption({ type }) {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
     id: type,
   });
   const style = transform ? {
@@ -156,7 +152,7 @@ function FeatureOption({type}){
 
 const modelOptions = ["Regression", "Clustering", "K-Means"]
 
-export function Level(){
+export function Level() {
   const [isDropped, setIsDropped] = useState(false);
   const [activeId, setActiveId] = useState(null);
   function handleDragEnd(event) {
@@ -168,19 +164,22 @@ export function Level(){
   }
   return (
     <DndContext onDragEnd={handleDragEnd}>
-    <h1 className='text-4xl text-center'>Fools Gold</h1>
-    <Box display="flex" alignItems="center" height="70vh" width="100vw" className='m-0'>
-        <Data />
-        <Model model={isDropped ? <ModelOption type={activeId}></ModelOption> : undefined}/>
-        <Train />
-        <Run />
-    </Box>
-    <Box display="flex" alignItems="center" height="30vh" width="100vw" className='m-auto'>
-      <Text>Drag a Model To Try it</Text>
-      {modelOptions.map((model) => {
-        return <ModelOption key={model} type={model} />
-      })}
-    </Box>
+      <h1 className='text-4xl text-center pb-10'>Fools Gold</h1>
+      <div className='w-full  h-2/3 inline-flex'>
+        <Box display="flex" alignItems="center" className='m-0 w-3/5'>
+          <Data />
+        </Box>
+        <Box display="flex" alignItems="center" className='m-0 w-2/5'>
+          <Model model={isDropped ? <ModelOption type={activeId}></ModelOption> : undefined} />
+          <TrainRun />
+        </Box>
+      </div>
+      <Box display="flex" alignItems="center" height="30vh" width="100vw" className='m-auto'>
+        <Text>Drag a Model To Try it</Text>
+        {modelOptions.map((model) => {
+          return <ModelOption key={model} type={model} />
+        })}
+      </Box>
 
     </DndContext>
   );
