385e03ba7b84a0ff42b759cf4bbcb46c34c86d3c
Yatin Kukreja <ykukreja@purdue.edu>
2024-02-18 00:41:01 -0800

    added story and small sandbox changes

--- frontend/src/components/sandbox.jsx
+++ frontend/src/components/sandbox.jsx
@@ -1,11 +1,7 @@
 import { Card, NumberInput, NumberInputField, NumberInputStepper, NumberDecrementStepper, NumberIncrementStepper, Text, Box, CardHeader, CardBody, Grid, GridItem, CardFooter, Flex, Button, Heading, TableContainer, TableCaption, Tabs, TabList, TabPanels, Tab, TabPanel, TabIndicator, FormControl, FormLabel, Select } from '@chakra-ui/react'
 import { DndContext, useDroppable, useDraggable } from '@dnd-kit/core';
 import { useEffect, useState } from 'react';
-import { getFirestore, onSnapshot, collection, doc } from "firebase/firestore";
 import { CSS } from '@dnd-kit/utilities';
-import { evalModel, generateData, trainModel } from '../helpers/callEndpoint';
-import { FeedbackBar } from './level';
-import { drop } from 'lodash';
 import Sand from '../sand.png';
 
 // A place to drop layers
@@ -62,16 +58,10 @@ function DroppableRow({ numDroppables, curLayers }) {
 
 function LayerBank({availableLayers}) {
     return (
-      <Tabs isFitted variant='unstyled' className="w-full border-2 border-slate-300 mt-2 ml-3 rounded-lg">
-      < TabList >
-        <Tab className="text-blue-800 hover:bg-gray-300">Layers</Tab>
-      </TabList >
-      <TabIndicator
-        className="border-b-2 border-blue-800 text-blue-800"
-      />
-
-      <TabPanels>
-        <TabPanel>
+      <>
+      <Heading className="font-lilitaOne text-center">
+        Layers
+      </Heading>
           <Grid h='200px' templateColumns='repeat(4, 1fr)' gap={4}>
             {availableLayers.map((layer) => {
               return (
@@ -81,18 +71,7 @@ function LayerBank({availableLayers}) {
               )
             })}
           </Grid>
-        </TabPanel>
-      </TabPanels>
-    </Tabs >
-        // <Grid templateColumns='repeat(4, 1fr)' gap={4}>
-        //       {availableLayers.map((layer) => {
-        //         return (
-        //           <GridItem key={layer} rowSpan={1} colSpan={1}>
-        //             <LayerOption key={layer} type={layer} />
-        //           </GridItem>
-        //         )
-        //       })}
-        // </Grid>
+          </>
       );
 }
   
@@ -100,6 +79,9 @@ function LayerOption({ type }) {
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
     id: type,
   });
+  const [dimensions, setDimensions] = new useState(6);
+  const format = (val) => `$` + val;
+  const parse = (val) => val.replace(/^\$/, '');
   const style = transform ? {
     transform: CSS.Translate.toString(transform),
   } : undefined;
@@ -119,7 +101,13 @@ function LayerOption({ type }) {
           inputLabel[type] && (
             <FormControl className='text-center'>
           <FormLabel className='ml-3 font'>Dimensions</FormLabel>
-            <NumberInput max={500} min={1} defaultValue={6} className='m-2'>
+            <NumberInput max={500} min={1} onChange={(valueString) => 
+            {
+                console.log("ON CHANGE\n");
+            setDimensions(parse(valueString));
+            }
+        }
+      value={format(dimensions)} className='m-2'>
           <NumberInputField />
         </NumberInput>
         </FormControl>
@@ -191,7 +179,7 @@ export default function Sandbox() {
     return (
         <>
         <DndContext onDragEnd={handleDragEnd} className="h-screen items-stretch">
-          <h1 className='text-4xl text-center pb-10 font-lilitaOne'>Sandbox</h1>
+          <h1 className='text-4xl text-center font-lilitaOne'>Sandbox</h1>
           {/* <div className='w-full h-full flex flex-col'> */}
             {/* <Box display="flex" alignItems="center" className='m-0 w-1/3 h-full inline-block'>
               <FeedbackBar />
